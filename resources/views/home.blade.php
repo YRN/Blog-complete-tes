84461a78d3b8cc2bbc6d68443dc74bddb9052c6b
yoanesrn <yoanesrn@gmail.com>
2019-11-11 09:19:54 +0700

    repair UI on admin/home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,48 +2,36 @@
 
 @section('content')
 
-
-<div class="col-lg-3">
-    <div class="card bg-primary mb-3 mx-auto">
-        <div class="card-header">
-            Published Posts
-        </div>
-        <div class="card-body">
-            <h1 class="text-center">
-                {{$posts_count}}
-            </h1>
-        </div>
+<div class="container">
+  <div class="card-deck">
+    <div class="card bg-primary">
+      <div class="card-header">Published Posts</div>
+      <div class="card-body">
+        <h1 class="text-center">{{ $posts_count }}</h1>
+      </div>
     </div>
-    <div class="card bg-danger mb-3 mr-0">
-        <div class="card-header">
-            Trashed Posts
-        </div>
-        <div class="card-body">
-            <h1 class="text-center">
-                {{$trashed_count}}
-            </h1>
-        </div>
+    <div class="card bg-danger">
+      <div class="card-header">Trashed Posts</div>
+      <div class="card-body">
+        <h1 class="text-center">{{ $trashed_count }}</h1>
+      </div>
     </div>
-    <div class="card bg-success mb-3">
-        <div class="card-header">
-            user
-        </div>
-        <div class="card-body">
-            <h1 class="text-center">
-                {{$users_count}}
-            </h1>
-        </div>
+    <div class="card bg-success">
+      <div class="card-header">user</div>
+      <div class="card-body">
+        <h1 class="text-center">{{ $users_count }}</h1>
+      </div>
     </div>
-    <div class="card bg-primary mb-3">
-        <div class="card-header">
-            categories
-        </div>
-        <div class="card-body">
-            <h1 class="text-center">
-                {{$categories_count}}
-            </h1>
-        </div>
+    <div class="card bg-primary">
+      <div class="card-header">categories</div>
+      <div class="card-body">
+        <h1 class="text-center">{{ $categories_count }}</h1>
+      </div>
     </div>
+  </div>
 </div>
+
+
+   
 
 @endsection
